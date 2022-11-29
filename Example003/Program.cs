ffbcc84c6775f4225e8c74e235f969e2e274c09d
Maria <gomodzyan@mail.ru>
2022-11-29 23:40:32 +0400

    add random

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,3 +1,5 @@
-﻿int num1 = 3;
-int num2 = 5;
+﻿int num1 = new Random().Next(1,10); // 1 to 9
+int num2 = new Random().Next(1,15);
+Console.WriteLine(num1);
+Console.WriteLine(num2);
 Console.WriteLine(num1+num2);
