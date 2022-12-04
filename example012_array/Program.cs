83c0cd6a3d755ba6ad1304c1307b0cf5e53d4e21
Maria <gomodzyan@mail.ru>
2022-12-04 12:31:05 +0400

    add Example012_array

---
+++ example012_array/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array ={1,12,31,4,44,15,16,17,15};
+
+int n=array.Length;
+int find = 15;
+
+int index=0;
+
+while(index<n)
+{
+	if(array[index]==find)
+	{
+		Console.WriteLine(index);
+		break;
+	}
+	index++;
+}
