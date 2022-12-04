2843ccc97d101ce2dbfdc4cbf3505a71387d1f40
Maria <gomodzyan@mail.ru>
2022-12-04 20:35:00 +0400

    add Exaple014

---
+++ example014/Program.cs
@@ -0,0 +1,30 @@
+﻿//for (int i=2; i<=10; i++)
+//{
+//	for (int j=2; j<=10; j++)
+//	{
+//		Console.WriteLine($"{i}x{j}={i*j}");
+//	}
+//	Console.WriteLine();
+//}
+
+string text ="– Я думаю, – сказал князь, улыбаясь, – что, "
+	+ "ежели бы вас послали вместо нашего милого Винценгероде, "
+	+ "вы бы взяли приступом согласие прусского короля. "
+	+ "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldVal, char newVal)
+{
+	string result = String.Empty;
+
+	int length = text.Length;
+	for (int i=0; i< length; i++)
+	{
+		if(text[i]==oldVal) result= result+$"{newVal}";
+		else result= result+$"{text[i]}";
+	}
+
+	return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
