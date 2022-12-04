ae9dcdfbf9eefda0d906c407cf17b8c28c3b119f
Maria <gomodzyan@mail.ru>
2022-12-04 21:05:11 +0400

    add homeWorkTwoLesson1№13

---
+++ homeWorkTwoLesson1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Задача 13 ");
+int a = 32;
+string strnum = Convert.ToString(a);
+
+if (strnum.Length > 2)
+{
+	Console.WriteLine(strnum[2]);
+}
+else
+{
+	Console.WriteLine("Третьей цифры нет");
+}
