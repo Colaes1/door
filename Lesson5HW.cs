03a2c4d4b05dd3f047851927434dab1a95d3acc7
Colaes1 <119356628+Colaes1@users.noreply.github.com>
2022-12-11 01:53:49 +0400

    Update Lesson5HW.cs

--- Lesson5HW.cs
+++ Lesson5HW.cs
@@ -29,3 +29,75 @@ void PrintArray(int[] numbers)
     Console.Write("]");
     Console.WriteLine();
 }
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int sum = 0;
+
+for (int z = 0; z < numbers.Length; z=z+2)
+sum=sum+numbers[z];
+Console.Write(sum);
+
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1,10);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+    
+    //Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+    Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int x = 0; x < numbers.Length; x++)
+{
+	if (numbers[x]> max)
+	{
+		max = numbers[x];
+	}
+	if (numbers[x] < min)
+	{
+		min = numbers[x];
+	}
+}
+double diff = max - min;
+Console.WriteLine($"Разница между максимальным и минимальным равна = " + (diff));
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        int randomNum = new Random().Next(1,10);
+		numbers[i] = Convert.ToDouble(randomNum)/10;
+    }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("| ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " | ");
+    }
+	
+    Console.WriteLine();
